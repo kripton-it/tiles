6347f00c180fb9df3390eca6bc003155e883f7d3
Mikhail Shardin <kriptonit1988@gmail.com>
2020-05-25 11:45:16 +0500

    Updates

---
+++ precache-manifest.6ba505896571f961ab2f4b4bf7ffa605.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9afc166cd7e259428bc77b24323201f2",
+    "url": "/tiles/index.html"
+  },
+  {
+    "revision": "dfc767993b1db4279e2b",
+    "url": "/tiles/static/css/main.dad1651d.chunk.css"
+  },
+  {
+    "revision": "b151ce353ae9d80291ab",
+    "url": "/tiles/static/js/2.3905c7b0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/tiles/static/js/2.3905c7b0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dfc767993b1db4279e2b",
+    "url": "/tiles/static/js/main.a92f4a0e.chunk.js"
+  },
+  {
+    "revision": "22bd060d9e8fc7959d39",
+    "url": "/tiles/static/js/runtime-main.83a3b221.js"
+  }
+]);
